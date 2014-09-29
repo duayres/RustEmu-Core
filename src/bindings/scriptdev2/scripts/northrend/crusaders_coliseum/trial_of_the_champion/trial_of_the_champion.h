0455c561aead935f0195350c08983701e36761a8
qvipka <qvipka@gmail.com>
2014-09-29 09:46:04 +0400

    added SD2 [3050] in repo

---
+++ src/bindings/scriptdev2/scripts/northrend/crusaders_coliseum/trial_of_the_champion/trial_of_the_champion.h
@@ -0,0 +1,17 @@
+/* This file is part of the ScriptDev2 Project. See AUTHORS file for Copyright information
+ * This program is free software licensed under GPL version 2
+ * Please see the included DOCS/LICENSE.TXT for more information */
+
+#ifndef DEF_TRIAL_OF_THE_CHAMPION_H
+#define DEF_TRIAL_OF_THE_CHAMPION_H
+
+enum
+{
+    MAX_ENCOUNTER                       = 3,
+
+    TYPE_GRAND_CHAMPIONS                = 0,
+    TYPE_ARGENT_CHAMPION                = 1,
+    TYPE_BLACK_KNIGHT                   = 2,
+};
+
+#endif
