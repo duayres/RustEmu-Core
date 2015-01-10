72ad4987c6cc6fdb98838acf11f2afe382642eaa
Schmoozerd <schmoozerd@scriptdev2.com>
2015-01-10 17:41:05 +0300

    [12783] Create function to load active entities. Note this function is considered WIP, and will require further work pending more features
    Ideas and loop to load all grids on a map by Neo2003.

--- src/game/ObjectMgr.h
+++ src/game/ObjectMgr.h
@@ -164,7 +164,7 @@ struct MangosStringLocale
     uint32 Emote;
 };
 
-typedef UNORDERED_MAP<uint32, CreatureData> CreatureDataMap;
+typedef UNORDERED_MAP<uint32 /*guid*/, CreatureData> CreatureDataMap;
 typedef CreatureDataMap::value_type CreatureDataPair;
 
 class FindCreatureData
@@ -793,6 +793,9 @@ class ObjectMgr
         void LoadVendors() { LoadVendors("npc_vendor", false); }
         void LoadTrainerTemplates();
         void LoadTrainers() { LoadTrainers("npc_trainer", false); }
+
+        /// @param _map Map* of the map for which to load active entities. If NULL active entities on continents are loaded
+        void LoadActiveEntities(Map* _map);
 
         void LoadVehicleAccessory();
 
