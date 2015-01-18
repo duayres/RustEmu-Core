0f45315a2f4d0133108f62a81d18df484e28e454
Schmoozerd <schmoozerd@cmangos.net>
2015-01-18 12:39:12 +0100

    [12823] Waypoint Commands: Improve export command, remove import command
    * Also finally drop the wpguid columns from creature_movement[_template]
    * Also remove all static spawns of id=1 (they are usually waypoints from the old system)
    * Fix a minor bug in WaypointManager than can result in a crash if bad params are passed

--- src/game/Chat.h
+++ src/game/Chat.h
@@ -554,7 +554,6 @@ class MANGOS_DLL_SPEC ChatHandler
         bool HandleWpModifyCommand(char* args);
         bool HandleWpShowCommand(char* args);
         bool HandleWpExportCommand(char* args);
-        bool HandleWpImportCommand(char* args);
 
         bool HandleHelpCommand(char* args);
         bool HandleCommandsCommand(char* args);
