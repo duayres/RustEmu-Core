4d19d743339481cfce6fadfe49a35b5f25485dee
kvipka <qvipka@gmail.com>
2015-02-24 11:07:36 +0300

    add missing sql - Instance Extend LFG

---
+++ sql/RE_updates/character_instance_extend_lfg.sql
@@ -0,0 +1,5 @@
+-- Instance Extend LFG
+-- Commit 020d4e346d38b961bf62
+
+ALTER TABLE `character_instance`
+    ADD COLUMN `extend` TINYINT(1) UNSIGNED NOT NULL DEFAULT '0' COMMENT 'Instance extend (bool)' AFTER `permanent`;
